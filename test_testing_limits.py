ffe82b1df83f740a79e7da1e39fd6ff7cc79bca6
benhigh22 <benjaming.high@gmail.com>
2016-02-16 16:37:04 -0500

    Bad spot, tried class of money

--- test_testing_limits.py
+++ test_testing_limits.py
@@ -1,32 +1,44 @@
 import unittest
 
-from testing_limits import euro_currency, multiple_currency_euro, usd_currency, multiple_currency_usd
+from testing_limits import Money
 
 
 class SingleEuroTestCase(unittest.TestCase):
 
     def test_currency_changes_for_one_number_to_euro(self):
-        self.assertEqual(euro_currency(1), .9)
+        self.assertEqual(Money().euro_currency(1), .9)
 
     def test_currency_changes_for_one_number_to_usd(self):
-        self.assertEqual(usd_currency(.9), 1)
+        self.assertEqual(Money().usd_currency(.9), 1)
 
     def test_currency_changes_for_a_number_that_has_a_decimal_to_only_2_places_to_euro(self):
-        self.assertEqual(euro_currency(.96), .86)
+        self.assertEqual(Money().euro_currency(.96), .86)
 
     def test_currency_changes_for_a_number_that_has_a_decimal_to_only_2_places_to_usd(self):
-        self.assertEqual(usd_currency(.86), .96)
+        self.assertEqual(Money().usd_currency(.86), .96)
+
+    def euro_test_currency_amount_is_negative(self):
+        self.assertEqual(Money().euro_currency(-10), -9)
+
+    def usd_test_currency_amount_is_negative(self):
+        self.assertEqual(Money().euro_currency(-1), -.9)
+
+    def test_if_not_an_integer(self):
+        self.assertEqual(Money().amt_not_valid("a"), "That's not a valid currency amount")
+
+    def test_if_amount_has_any_characters(self):
+        self.assertEqual(Money().amt_no_chars(""), "That's not a valid currency amount")
 
 class MultipleEuroTestCase(unittest.TestCase):
 
     def test_currency_changes_for_several_numbers_returns_list_in_euros(self):
-        self.assertEqual(multiple_currency_euro([1, 2, 3]), [.9, 1.8, 2.7])
+        self.assertEqual(Money().multiple_currency_euro([1, 2, 3]), [.9, 1.8, 2.7])
 
     def test_currency_changes_for_several_numbers_returns_list_in_usd(self):
-        self.assertEqual(multiple_currency_usd([.9, 1.8, 2.7]), [1, 2, 3])
+        self.assertEqual(Money().multiple_currency_usd([.9, 1.8, 2.7]), [1, 2, 3])
 
     def test_currency_changes_for_several_numbers_returns_list_in_euros_with_decimals(self):
-        self.assertEqual(multiple_currency_euro([1.2, 190.75, 4.67]), [1.08, 171.68, 4.20])
+        self.assertEqual(Money().multiple_currency_euro([1.2, 190.75, 4.67]), [1.08, 171.68, 4.20])
 
     def test_currency_changes_for_several_numbers_returns_list_in_usd_with_decimals(self):
-        self.assertEqual(multiple_currency_usd([5.46, 10.23]), [6.07, 11.37])
+        self.assertEqual(Money().multiple_currency_usd([5.46, 10.23]), [6.07, 11.37])
--- testing_limits.py
+++ testing_limits.py
@@ -1,15 +1,29 @@
 
+class Money:
 
-def euro_currency(value):
-    euro = value * .9
-    return round(euro, 2)
+    def __init__(self, amount=0, currency=None):
+        self.amount = amount
+        self.currency = currency
 
-def usd_currency(value):
-    usd = value / .9
-    return round(usd, 2)
+    def euro_currency(self, amount):
+        euro = amount * .9
+        return round(euro, 2)
 
-def multiple_currency_euro(euro_list):
-    return [round((amount * .9), 2) for amount in euro_list]
+    def usd_currency(self, amount):
+        usd = amount / .9
+        return round(usd, 2)
 
-def multiple_currency_usd(usd_list):
-    return [round((amount / .9), 2) for amount in usd_list]
+    def amt_not_valid(self, amount):
+        if amount.isalpha():
+            return "That's not a valid currency amount"
+
+    def amt_no_chars(self, amount):
+        if len(amount) == 0:
+            return "That's not a valid currency amount"
+
+    def multiple_currency_euro(self, euro_list):
+        return [round((amount * .9), 2) for amount in euro_list]
+
+    def multiple_currency_usd(self, usd_list):
+        return [round((amount / .9), 2) for amount in usd_list]
+
